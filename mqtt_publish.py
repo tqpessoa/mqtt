cad97f4e842d0e45718b032f0242424cebc6370f
Talles Pessoa <eng.quintao@gmail.com>
2018-06-12 14:13:29 -0300

    Update mqtt_publish.py

--- mqtt_publish.py
+++ mqtt_publish.py
@@ -3,6 +3,6 @@
 
 import paho.mqtt.publish as publish
 
-publish.single("CoreElectronics/test", "Hello", hostname="test.mosquitto.org")
-publish.single("CoreElectronics/topic", "World!", hostname="test.mosquitto.org")
+publish.single("IOT/test", "Hello", hostname="test.mosquitto.org")
+publish.single("IOT/topic", "World!", hostname="test.mosquitto.org")
 print("Done")
